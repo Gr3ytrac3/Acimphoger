6960a8f6f397af952da7bab6512709d7655c17a3
Nolan Levi <cyberdevhq@proton.me>
2025-05-09 01:25:58 +0100

    Changed the author's name

--- acimphoger.py
+++ acimphoger.py
@@ -15,7 +15,7 @@ def print_banner():
                        /_/          /____/             
     """
     print(banner)
-    print("Author      : Cyberdev")
+    print("Author      : CyberdevHq")
     print("Version     : 1.0.0")
     print("GitHub      : Gr3ytrac3")
     print("Tool        : Generate Fake IDs & Strong Passwords\n")
